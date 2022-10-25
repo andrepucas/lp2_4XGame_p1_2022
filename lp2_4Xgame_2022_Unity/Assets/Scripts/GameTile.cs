9b5d13a74a53eb1a36f336360d2fbe077f668bb9
AfonsoLage-boop <afomsosantoslage@gmail.com>
2022-10-25 18:15:50 +0100

    Add Scripts folder & GameTile script

---
+++ lp2_4Xgame_2022_Unity/Assets/Scripts/GameTile.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class GameTile : MonoBehaviour
+{
+    /// <summary>
+    /// Read only self implemented property that stores the monetary value of
+    /// each game tile.
+    /// </summary>
+    /// <value>Monetary value of the game tile.</value>
+    public abstract int Coin {get;}
+
+    /// <summary>
+    /// Read only self implemented property that stores the food production 
+    /// value of each game tile.
+    /// </summary>
+    /// <value>Food Production of the game tile.</value>
+    public abstract int Food {get;}
+}
