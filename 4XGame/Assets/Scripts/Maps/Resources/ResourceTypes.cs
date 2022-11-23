93e49871dee7fa9dd28fff2cc111cd86a5a79f3f
AfonsoLage-boop <afomsosantoslage@gmail.com>
2022-11-23 20:38:04 +0000

    Add Enum with resource types

---
+++ 4XGame/Assets/Scripts/Maps/Resources/ResourceTypes.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum ResourceTypes
+{
+    Animals,
+    FossilFuel,
+    Metals,
+    Plants,
+    Pollution
+}
