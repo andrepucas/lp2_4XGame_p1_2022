ff2f8bc30daa4d9e4cca1f89c33a288f779ec31d
AfonsoLage-boop <afomsosantoslage@gmail.com>
2022-11-28 16:34:05 +0000

    Map displays resource sprites in appropriate cells

--- 4XGame/Assets/Scripts/Maps/GridCell.cs
+++ 4XGame/Assets/Scripts/Maps/GridCell.cs
@@ -5,16 +5,24 @@ using UnityEngine;
 public class GridCell : MonoBehaviour
 {
     private GameTile _tile;
-    // [SerializeField] private GameObject _animalsSprite;
-    // [SerializeField] private GameObject _fossilFuelSprite;
-    // [SerializeField] private GameObject _luxurySprite;
-    // [SerializeField] private GameObject _metalsSprite;
-    // [SerializeField] private GameObject _plantsSprite;
-    // [SerializeField] private GameObject _pollutionSprite;
+    [SerializeField] private GameObject _animalsSprite;
+    [SerializeField] private GameObject _fossilFuelSprite;
+    [SerializeField] private GameObject _luxurySprite;
+    [SerializeField] private GameObject _metalsSprite;
+    [SerializeField] private GameObject _plantsSprite;
+    [SerializeField] private GameObject _pollutionSprite;
 
     public void Initialize(GameTile p_tile)
     {
         _tile = p_tile;
+        _animalsSprite.SetActive(false);
+        _fossilFuelSprite.SetActive(false);
+        _luxurySprite.SetActive(false);
+        _metalsSprite.SetActive(false);
+        _plantsSprite.SetActive(false);
+        _pollutionSprite.SetActive(false);
+
+        EnableResourceSprites(_tile);
     }
 
     public void OnClick()
@@ -22,5 +30,45 @@ public class GridCell : MonoBehaviour
         Debug.Log($"Coin: {_tile.Coin}, Food: {_tile.Food}");
     }
 
+    public void EnableResourceSprites(GameTile p_tile)
+    {
+        foreach (Resource resource in p_tile.Resources)
+        {
+            switch (resource.Type)
+            {
 
+                case ResourceTypes.Animals:
+
+                    _animalsSprite.SetActive(true);
+                    break;
+
+                case ResourceTypes.FossilFuel:
+
+                    _fossilFuelSprite.SetActive(true);
+                    break;
+
+                case ResourceTypes.Luxury:
+
+                    _luxurySprite.SetActive(true);
+                    break;
+
+                case ResourceTypes.Metals:
+
+                    _metalsSprite.SetActive(true);
+                    break;
+
+                case ResourceTypes.Plants:
+
+                    _plantsSprite.SetActive(true);
+                    break;
+
+                case ResourceTypes.Pollution:
+
+                    _pollutionSprite.SetActive(true);
+                    break;
+            }
+        }
+    }
 }
+
+
